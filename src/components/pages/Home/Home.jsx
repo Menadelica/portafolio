b6c7d382a9c02946bceea91a7b029ed1dd79aad3
Menadelica <ximenanorena50@gmail.com>
2025-04-30 11:57:51 -0500

    first commit

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+ import './Home.css'
+ export function Home(){
+    return(
+
+        <>  
+
+        <section className="container">
+            <div className="row justify-content-center">
+                <div className="col-4">
+                    <h3>Leidy Jimena Noreña Quiceno</h3>
+                    <hr />
+                    <img src="./src/assets/img/mena.jpg" alt="foto" className="img-fluid rounded shadow foto"/>
+                    <p>
+                    Soy Jimena, desarrolladora de software en formación, creadora y emprendedora apasionada por la tecnología, la educación consciente y el impacto social. Me especializo en proyectos que integran programación, inteligencia artificial, sostenibilidad y creatividad, con el propósito de construir soluciones humanas, accesibles y transformadoras que conecten saberes y generen bienestar.
+                    </p>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
